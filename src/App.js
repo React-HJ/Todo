02fd2e3c8d6f106559bdd97f8d933324113c860c
React-HJ <128790472+React-HJ@users.noreply.github.com>
2025-12-24 13:27:18 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './component/Header';
+import TodoEditor from './component/TodoEditor';
+import TodoList from './component/TodoList';
+
+function App() {
+  const [Todo, settodo] = useState([]);
+  return (
+  <div className="App">
+    <Header />
+    <TodoEditor />
+    <TodoList />
+  </div>
+  );
+}
+
+export default App;
